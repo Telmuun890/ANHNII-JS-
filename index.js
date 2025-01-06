848453507ecbf2cbd69e64c200c256bcfc3a9716
24LP4494 24LP4494 <24LP4494@G303-CC0128.local>
2025-01-06 11:53:37 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,394 @@
+// var greeting = 'Hellow World'; 
+
+
+// console.log(greeting); 
+
+
+
+// var num = 1 ; 
+// var text = 'hello' ;
+// var isTrue = false ;
+
+// def = num + text;
+
+// console.log (def)
+
+
+
+
+// for(let i = 1; i <=5; i++) { 
+//     console.log(i);
+// }
+
+
+
+
+// var text =''; 
+// text = text + '*'; 
+// console.log(text); 
+
+// text = text+ '*';
+// console.log(text)
+
+// var text = '*' + text
+// console.log( text )
+
+// var text = '*' + text
+// console.log( text )
+
+// var text = '*' + text 
+// console.log( text )
+
+
+// for( let i = 1; i <=5; i++){ 
+//     console.log(i);
+// }
+
+
+// var text =''; 
+// for( var i = 0; i < 5 ; i++){ 
+//     text  += '*'
+//     console.log(text )
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+// for ( let i = 1; i <= 100; i++ )  {
+//     if (i % 2 !==0 )
+//         console.log(i)
+// }
+// for ( let i = 1; i <= 100; i++ )  {
+//     if (i % 2 ===0 )
+//         console.log(i)
+// }
+
+
+// for( let i= 1; i <= 100; i++){
+//     if( i * 2 ===0 )
+//         console.log(i)
+//     }
+
+
+// var sum = 0 
+// for( var i = 1; i <= 100 ; i++ ){ 
+//     sum += i
+//     console.log(sum)
+// }
+
+
+// var multiple = 1;
+
+//  for( var i = 1;  i <= 10; i++ ){ 
+//     if( i % 2 == 0 ) { 
+//         multiple *= i 
+//     }
+//  }
+//  console.log( multiple)
+
+// 121 -> palindrome
+// hadgaladah -> palindrome 
+// golog -> palindrome 
+
+// var palindrome = ' golog '; { 
+//     for (let i = 0 ; i < ) 
+// } 
+
+
+// let str = 'tmt';
+// let n = str.length;
+
+// console.log(checkPalindrome(str,n));
+
+// function checkPalindrome(str,n){
+//     for(let i=0; i < n; i++){
+//         if(str[i] != str[n-1-i]){
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+// var text = ' harah ' ;
+// var reverse = ''; 
+// for( var i = text.length - 1; i >= 0; i--){ 
+//     reverse = reverse = text[i];
+// }
+// if( text == reverse){ console.log(true)}
+// else{ console.log( false)}
+
+// var too = 2;
+// var zereg = 3; 
+// var result = 1; 
+// for( let i = 1; i <= zereg ; i++) { 
+//     too = result * too 
+//     console.log ( result)
+// }
+
+
+// var text = 'harah';
+// let l = 0;
+// let r = text.length - 1;
+
+// for (l; l < text.length / 2; l++) {
+//     if (text[l] != text[r]) {
+//         console.log(false);
+//     }
+//     r--;
+// }
+
+
+// let n = 1;
+// let isPrimeNumber = true;
+
+// if (n == 1) isPrimeNumber = false
+
+// for (let i = 2; i < n; i++) {
+//     if (n % i == 0) {
+//         isPrimeNumber = false
+//     }
+// }
+
+// console.log(isPrimeNumber)
+
+
+
+// var text =''; 
+// for( var i = 0; i < 5 ; i++){ 
+//     text  += '*'
+//     console.log(text )
+// }
+
+
+// for (let i = 1; i <= 5; i++) {
+//     let text = '*'
+//     for (let j = 0; j <= 5; j++) {
+//         if (i >= j) {
+//             text = text + '*'
+//         }
+//         else {
+//             text = text + ''
+//         }
+//         console.log(i, j)
+//     }
+
+// }
+
+
+// for(i = 1; i <= 5; i++) {
+//     let text =''
+//     for(j = 0; j < 5; j++) {
+
+// if(i > j) {
+//     text = text + '*'
+// }
+// else{
+//     text = text + ' '}
+//     }
+//     console.log(text)
+
+// }
+
+//  for( let i = 1; i <=5; i++){ 
+//     console.log(i);
+// }
+
+
+// var text =''; 
+// for( var i = 0; i < 5 ; i++){ 
+//     text  += '*'
+//     console.log(text )
+// }
+// var text =''; 
+// for( var i = 0; i < 5 ; i++){ 
+//     text  += '*'
+//     console.log(text )
+// }
+
+// mod__
+// var row, col, n=5
+// var pattern = ""
+// for(row= 1; row<=n; row++){
+//     for(col=1;col<=n-row;col++){
+//         pattern +=" "
+//     }
+//     for(col=1;col<=row;col++){
+//         pattern +="* "
+//     }
+//     pattern +="\n"
+// }
+// console.log(pattern);
+//  zurh
+// function heartPatttern(n) {
+//     for(let i=n/2;i<n;i +=2){
+//         let line =
+//         ' '.repeat((n-i)/2)+
+//         '*'.repeat(i)+
+//         ' '.repeat(n-i)+
+//         '*'.repeat(i);
+//         console.log(line);
+//     }
+//     for (let i=n;i>=1;i--){
+//         let line =
+//         ' '.repeat(n-i) +
+//         '*'.repeat(2*i-1);
+//         console.log(line)
+//     }
+// }
+// function heartPatttern(n) {
+//     for(let i=n/2;i<n;i +=2){
+//         let line =
+//         ' '.repeat((n-i)/2)+
+//         '*'.repeat(i)+
+//         ' '.repeat(n-i)+
+//         '*'.repeat(i);
+//         console.log(line);
+//     }
+//     for (let i=n;i>=1;i--){
+//         let line =
+//         ' '.repeat(n-i) +
+//         '*'.repeat(2*i-1);
+//         console.log(line)
+//     }
+// }
+// heartPatttern(8)
+
+
+
+
+// const number = [10, 15, 20  ];
+// let max = 0 ;
+// for(let i = 0 ; i < number.length; i++ ){
+//     if(max > number[i]){
+//     }
+// }
+// console.log(max)
+
+// if( a > b && a > c ){
+//     console.log(a)
+// }
+// if( b > a && b > c){
+//     console.log(b)
+// } 
+// if( c > a && c >b ){
+//     console.log(c)
+// }
+
+// if( a > b && a > c){
+//     console.log(a)
+// }
+// else if(b > a && b > c){
+//     console.log(b)
+// }
+// else if( c > a && c > b){ 
+//     console.log(c)
+// }
+
+// var a = 5
+// var b = 6
+// var c = 7
+// var d = 8
+
+// if(a > b && a > c && a < d){
+//     console.log(a)
+// }
+// else if( a > b && a > d && a < c){
+//     console.log(a)
+// }
+// else if(a > b && a > c && a< b){
+//     console.log(a)
+// }
+// else if( b > a && b > c && b < d){
+//     console.log(b)
+// }
+// else if( b > a && b > d && b < c){ 
+//     console.log(b)
+// }  
+// else if( b > c && b > d && b < d ){
+//     console.log(b)
+// }
+// else if ( c > a && c > b && c < d ){
+//     console.log(c)
+// }
+// else if( c >a && c >d && c < b){
+//     console.log(c)
+// }
+// else if( c > b && c >d && c < a){
+//     console.log(c)
+// }
+// else if ( d > a && d > b && d < c){
+//     console.log(d)
+// }
+// else if (d > a && d > c && d< b ){
+//     console.log(d)
+// }
+// else if ( d > b && d > c && d < a){
+//     console.log(d)
+// } 
+
+
+// //two sum 
+// const nums = [2, 7, 7, 15];
+
+// for (var i = 0; i <= nums.length; i++) {
+//     for (var j = 0; j <= nums.length; j++) {
+//         if (nums[i] + nums[j]) {
+//             console.log(i, j)
+//         }
+//     }
+// }
+
+
+// function isLeapYear( year ){
+//     if(year % 4== 0 && year % 400 == 0 || year % 100 != 0)
+//         return true
+// } 
+//     return false
+//     console.log(year)
+
+
+//1-1000 HURTEL 3 T HUVAAGDDG TOO 
+
+// for( let i = 1; i < 1000 ; i++ ){
+//     if( i % 3 === 0 ){
+//         console.log(i )
+//     }
+// }
+
+//2 
+
+// for( let i = 0  ; i < 1000; i+=3 ){
+    
+// console.log(i)
+// }
+
+ //3
+
+//  let a = 999;  
+//  let b = 3;
+//  let c =333; 
+//  let sum = (a + b)* c/2
+// {console.log(sum)}
+
+const numbers = [1, 9, 14, 2, 49];
+for (let i = 0; i < numbers.length; i++) {
+    for (let k = i+1; k < numbers.length; k++) {
+        if (numbers[i] < numbers[k]) { 
+            let r = numbers[i]; 
+            numbers[i] = numbers[k] 
+             numbers[k] = r 
+             console.log(numbers)
+            }
+            else{
+                continue
+            }
+        }
+    }
